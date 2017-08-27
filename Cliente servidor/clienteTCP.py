61241f7e4a71fbc8ff13b655d4cf466df892cd74
mdbelloc <mdbelloc@unal.edu.co>
2017-08-27 13:40:24 -0500

    Cliente actualizado

--- Cliente servidor/clienteTCP.py
+++ Cliente servidor/clienteTCP.py
@@ -10,18 +10,23 @@ def makeConnection( host, port, destino ):
     client.send('GET /'+ destino + ' HTTP/1.0\r\nHost: \r\n\r\n')
     
     # Recibimos la respuesta del servidor
-    response = client.recv(4096)
-    print "Timeout : " + str(client.gettimeout())
     
-    print response
-
+    print ("Timeout : " + str(client.gettimeout()))
+    response = (client.recv(4096))
+    #print (response)
+    lista = (client.recv(4096)).split("<body>")
+    #print client.recv(4096)
+    #print lista[1]
+    lista2= lista[1].split("</body>")
+    print lista2[0]
 
 
 host = "1"
 while (len(host)>0):
     host = raw_input("address: ")
     port = int(raw_input("port: "))
-    url = raw_input("destino: ")
+    url = str(raw_input("destino: "))
     if(len(host)>1):
         makeConnection(host,port, url)
+        
 
